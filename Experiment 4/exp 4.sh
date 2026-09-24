9f449b3f4af11db46dc0a39c1df26cab33e686bb
Harshdeep singh <145020109+harshdeep2w2w@users.noreply.github.com>
2026-09-24 21:55:24 +0530

    Create exp 4.sh

---
+++ Experiment 4/exp 4.sh
@@ -0,0 +1,23 @@
+===== X.509 SELF-SIGNED CERTIFICATE EXPERIMENT =====
+
+----- OpenSSL Version -----
+OpenSSL 3.6.2 7 Apr 2026 (Library: OpenSSL 3.6.2 7 Apr 2026)
+
+----- Certificate Details -----
+subject=C=IN, ST=Jammu, L=Jammu, O=MIET, OU=CSE, CN=localhost
+issuer=C=IN, ST=Jammu, L=Jammu, O=MIET, OU=CSE, CN=localhost
+notBefore=Sep 24 15:55:01 2026 GMT
+notAfter=Sep 24 15:55:01 2027 GMT
+serial=3475C67BE8A0B3562E3B9D063B59E679DE84455F
+
+----- Subject Alternative Name -----
+X509v3 Subject Alternative Name: 
+    DNS:localhost, IP Address:127.0.0.1
+
+----- Certificate Verification -----
+certificate.crt: OK
+
+----- SHA-256 Fingerprint -----
+sha256 Fingerprint=AE:6F:A9:20:23:24:7C:34:B8:25:0C:D3:73:45:90:E2:B3:FD:00:D2:14:94:86:B1:05:BF:33:83:5A:75:76:71
+
+===== REPORT COMPLETE =====
